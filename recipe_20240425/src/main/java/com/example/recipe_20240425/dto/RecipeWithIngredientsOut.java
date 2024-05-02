f54975cf431fad73d41fb34c9b5b14d44e591fb8
sigitabra <sigita.zutautaite@gmail.com>
2024-05-02 21:13:31 +0300

    Version 4

--- recipe_20240425/src/main/java/com/example/recipe_20240425/dto/RecipeWithIngredientsOut.java
+++ recipe_20240425/src/main/java/com/example/recipe_20240425/dto/RecipeWithIngredientsOut.java
@@ -1,9 +1,11 @@
 package com.example.recipe_20240425.dto;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 public class RecipeWithIngredientsOut extends RecipeOut {
 
